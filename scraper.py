2c0a8215b6ba8732b1fe1535c06ac58d2697daf6
Nisarg Mehta <nm555@cam.ac.uk>
2017-09-21 17:21:19 +0100

    Added comments, made more readable

--- scraper.py
+++ scraper.py
@@ -7,39 +7,34 @@ url = "/get/lists/subscribers"
 
 link = base_url + url
 
-page = urllib2.urlopen(link)
-soup = BeautifulSoup(page,"html.parser")
+page = urllib2.urlopen(link) # open the page
+soup = BeautifulSoup(page,"html.parser") # parse it using BS
 
-tables = soup.find_all('table')
+tables = soup.find_all('table') # extract the two tables from the page
 resource_information = tables[0]
-parameters = tables[1]
+parameters = tables[1] # allocate individual tables to separate variables
 
-td_tags = resource_information.find_all('td')
-resource_info_list = []
-for tag in td_tags:
-    resource_info_list.append(tag.text)
-resource_info = dict(zip(resource_info_list[::2], resource_info_list[1::2]))
+td_tags = resource_information.find_all('td') # extract all td tags from the table - all entries in the table
+resource_info_list = [tag.text for tag in td_tags] # empty list to store each value
+resource_info = dict(zip(resource_info_list[::2], resource_info_list[1::2])) # create a dictionary with odd values as keys and even values as vals.
 print resource_info, "\n------------------------------"
 
-td_tags = parameters.find_all('td')
-temp_info = []
-for tag in td_tags:
-    temp_info.append(tag.text)
+td_tags = parameters.find_all('td') # n by 5 table - extract all parameters
+temp_info = [tag.text for tag in td_tags]
 n = len(temp_info) / 5
-params = []
-frames = []
+
+frames = [] # list to store batches of 5 parameters
 for i in xrange(0,len(temp_info),5):
     frame = temp_info[i:i+5]
     frames.append(frame)
 names = frames[0]
 del frames[0]
-for frame in frames:
-    params.append(dict(zip(names,frame)))
+params = [dict(zip(names,frame)) for frame in frames] # create a list of dictionaries, where each dictionary contains the extracted parameters
 
 for param in params:
     print param, "\n---------------------------"
 
-urls = soup.find_all("cite")
+urls = soup.find_all("cite") # extract the resource url
 for url in urls:
     if url.text.startswith('http'):
         resource_url = url.text
